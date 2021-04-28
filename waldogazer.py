50f15afab5ff51ef414bd99fd6cee55d09da50de
Aroweeri <dylanlsutton@gmail.com>
2021-04-28 19:07:12 -0400

    Add controls to customize number of rows and columns.

--- waldogazer.py
+++ waldogazer.py
@@ -8,13 +8,12 @@ import gridimage
 class WaldoGazer(Gtk.Window):
 
 	gridImage = None
-	numRows=4
-	numColumns=4
+	numRows=3
+	numCols=3
 	pixbuf=None
 	pixbufToDisplay=None
 	scaledPixbuf=None
 	img=None
-	scrolledWindow=None
 
 	def __init__(self, pixbuf, img):
 		Gtk.Window.__init__(self)
@@ -29,12 +28,15 @@ class WaldoGazer(Gtk.Window):
 
 		#build image placeholder
 		self.pixbuf = GdkPixbuf.Pixbuf.new_from_file("image.jpg")
-		self.pixbufToDisplay = self.pixbuf
-		self.img = Gtk.Image.new_from_pixbuf(self.pixbuf)
+		self.img = Gtk.Image()
+		self.reload()
 
-		self.gridImage = gridimage.GridImage(self.pixbuf,
-		                                     self.numRows,
-		                                     self.numColumns);
+		rowsScale = Gtk.Scale.new_with_range(Gtk.Orientation.VERTICAL, 1, 100, 1);
+		colsScale = Gtk.Scale.new_with_range(Gtk.Orientation.HORIZONTAL, 1, 100, 1);
+		rowsScale.connect("value-changed", self.on_rows_changed); 
+		colsScale.connect("value-changed", self.on_cols_changed); 
+		rowsScale.set_value(self.numRows)
+		colsScale.set_value(self.numCols)
 
 		hbox = Gtk.HBox.new(Gtk.Orientation.HORIZONTAL, 10)
 		nextButton = Gtk.Button.new_with_label("Next")
@@ -46,6 +48,8 @@ class WaldoGazer(Gtk.Window):
 		hbox.add(file_chooser_button)
 		hbox.add(nextButton)
 		hbox.add(prevButton)
+		hbox.add(rowsScale);
+		hbox.add(colsScale);
 		paned.pack1(hbox, True, True)
 		paned.pack2(scrolledWindow, True, True)
 
@@ -56,10 +60,11 @@ class WaldoGazer(Gtk.Window):
 
 	def on_file_selected(self, button):
 		self.pixbuf = GdkPixbuf.Pixbuf.new_from_file(button.get_filename())
+		self.reload()
+
+	def reload(self):
 		self.pixbufToDisplay = self.pixbuf
-		self.gridImage = gridimage.GridImage(self.pixbuf,
-		                                     self.numRows,
-		                                     self.numColumns)
+		self.gridImage = gridimage.GridImage(self.pixbuf, self.numRows, self.numCols)
 		self.img.set_from_pixbuf(self.pixbufToDisplay)
 
 	def scale_image(self, pixbuf, width, height):
@@ -86,7 +91,6 @@ class WaldoGazer(Gtk.Window):
 		newHeight = imgHeight*aspect
 
 		self.scaledPixbuf = self.scale_image(self.pixbufToDisplay, newWidth, newHeight)
-
 		self.img.set_from_pixbuf(self.scaledPixbuf)
 
 	def on_next_clicked(self, widget):
@@ -96,3 +100,11 @@ class WaldoGazer(Gtk.Window):
 	def on_prev_clicked(self, widget):
 		self.pixbufToDisplay = self.gridImage.getPrev()
 		self.img.set_from_pixbuf(self.pixbufToDisplay)
+
+	def on_rows_changed(self, scale):
+		self.numRows = int(scale.get_value())
+		self.reload()
+
+	def on_cols_changed(self, scale):
+		self.numCols = int(scale.get_value())
+		self.reload()
