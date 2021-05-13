6af0223c9775aed4841302a0050a27f63530ec5c
Aroweeri <dylanlsutton@gmail.com>
2021-05-12 21:16:03 -0400

    Add recent file.

--- waldogazer.py
+++ waldogazer.py
@@ -26,7 +26,7 @@ class WaldoGazer(Gtk.Window):
 		file_chooser_button = Gtk.FileChooserButton()
 		file_chooser_button.connect("file-set", self.on_file_selected)
 
-		self.pixbuf = GdkPixbuf.Pixbuf.new_from_file("image.jpg")
+		self.pixbuf = GdkPixbuf.Pixbuf.new_from_file("blank.png")
 		self.img = scaleimage.ScaleImage(self.pixbuf)
 		self.img.set_vexpand(True)
 		self.reload()
@@ -37,6 +37,9 @@ class WaldoGazer(Gtk.Window):
 		colsScale.connect("value-changed", self.on_cols_changed); 
 		rowsScale.set_value(self.numRows)
 		colsScale.set_value(self.numCols)
+
+		hbox = Gtk.HBox.new(Gtk.Orientation.HORIZONTAL, 3)
+		hbox.pack_start(Gtk.Label.new("Recent:"), True, True, 0)
 
 		nextButton = Gtk.Button.new_with_label("Next")
 		nextButton.connect("clicked", self.on_next_clicked)
@@ -54,14 +57,17 @@ class WaldoGazer(Gtk.Window):
 		grid.attach(prevButton, 2, 0, 1, 1)
 		grid.attach(rowsScale, 3, 0, 1, 1)
 		grid.attach(colsScale, 4, 0, 1, 1)
-		grid.attach(self.img, 0, 1, 5, 1)
+		grid.attach(hbox, 0, 1, 5, 1)
+		grid.attach(self.img, 0, 2, 5, 1)
 
 		self.connect("destroy", Gtk.main_quit)
 
 		self.add(grid)
 
 	def on_file_selected(self, button):
-		self.pixbuf = GdkPixbuf.Pixbuf.new_from_file(button.get_filename())
+		filename = button.get_filename()
+		self.append_recent_file(filename)
+		self.pixbuf = GdkPixbuf.Pixbuf.new_from_file(filename)
 		self.reload()
 
 	def on_next_clicked(self, widget):
@@ -81,6 +87,39 @@ class WaldoGazer(Gtk.Window):
 		self.reload()
 
 	def reload(self):
+
+		if(self.pixbuf == None):
+			return
+
 		self.pixbufToDisplay = self.pixbuf
 		self.gridImage = gridimage.GridImage(self.pixbuf, self.numRows, self.numCols)
 		self.img.change_image(self.pixbufToDisplay)
+	
+	def append_recent_file(self, filename):
+		maxRecentFiles=10
+		recentFiles = []
+
+		#store current contents
+		recent_file = open("recent.txt", "r")
+		for line in recent_file:
+			if(line != "\n"):
+				recentFiles.append(line.rstrip())
+		recent_file.close()
+
+		#shift lines backward one, add passed filename at end
+		numRecentFiles=len(recentFiles)
+		if(numRecentFiles >= maxRecentFiles):
+			for i in range(numRecentFiles-1):
+				recentFiles[i] = recentFiles[i+1]
+			recentFiles[numRecentFiles-1] = filename
+		else:
+			recentFiles.append(filename)
+
+		#write contents back
+		recent_file = open("recent.txt", "w")
+		for file in recentFiles:
+			recent_file.write(file + str("\n"))
+		recent_file.close()
+
+
+
